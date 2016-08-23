4aa16a4c401624d357c6068c40e9b5a8cadba9db
s-agarwal <s-agarwal@test>
2016-08-23 18:16:06 +0900

    Duplicate Test

---
+++ src/main/test/java/circleci/DemoApplicationTest.java
@@ -0,0 +1,27 @@
+package circleci;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import org.junit.runner.RunWith;
+import static org.hamcrest.CoreMatchers.nullValue;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import circleci.DemoApplication;
+import circleci.controller.CircleController;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoApplication.class)
+public class DemoApplicationTest {
+	
+	@Autowired
+	CircleController controller;
+	
+	@Test
+	public void contextLoaded() {
+		assertThat(controller, nullValue());
+	}
+}
